1bcd6c32666809a726aa3b512385e3fa79c2077f
iysaofficial <youngscientist.iysa@gmail.com>
2024-03-21 14:51:34 +0700

    revisi hero

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -6,7 +6,7 @@ const HeroComp = () =>{
                 <img src="./assets/images/IYBO.png" alt="" />
                 <div className="hero-text">
                     <h1>International Youth Biology Olympiad</h1>
-                    <p>Registration is now open for the International Youth Biology Olympiad! Join this prestigious event and gain an unforgettable biological experience!</p>
+                    <p>Registration is now open for the International Youth Biology Olympiad! Join this prestigious event and gain an unforgettable experience!</p>
                     <a href="https://registrasi-iybo-2024.vercel.app/" target="_blank">Registration</a>
                 </div>
             </div>
