e72ebb10966197709f9caddce6a09e058c70bfd1
iysaofficial <youngscientist.iysa@gmail.com>
2024-05-03 16:35:55 +0700

    repair about section

--- src/components/home/AboutComp.jsx
+++ src/components/home/AboutComp.jsx
@@ -11,7 +11,7 @@ const AboutComp = () =>{
                     <p>An event for the achievement of talented young academics from various countries around the world in solving global problems</p>
                 </div>
                 <div className="box">
-                    <img src="./assets/images/IYBO.png" alt=""/>
+                    <img src="./assets/images/logo/IYBO.png" alt=""/>
                     <h3>Competition Category</h3>
                     <p>Online competition for Indonesia and International Participants</p>
                 </div>
